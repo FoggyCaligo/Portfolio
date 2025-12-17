8adf78eaf72f3b00c75e11eadc9ef37ab28dd4f3
신재용 <dkdlwkr16@gmail.com>
2025-12-17 17:34:15 +0900

    add - skills page

---
+++ src/components/SkillsPage/Format.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="w-full pt-4">
+    <p class="w-full flex text-start font-bold text-xl text-surface">{{ title }}</p>    
+    <div class="w-full flex text-start font-thin text-lg text-surface">
+      <slot></slot>
+    </div>
+  </div>
+</template>
