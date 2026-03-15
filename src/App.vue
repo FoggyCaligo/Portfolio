169e6e6485e5d3434fe5e52d02936e8493db913e
FoggyCaligo <dkdlwkr16@gmail.com>
2026-03-15 20:56:26 +0900

    코드 단순화

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
   import Navbar from './components/Navbar.vue'
-  const mobilePadding = 'px-5'
-  const customPadding = 'md:px-20 px-5 md:px-20 py-5'
 
 </script>
   
 <template>
-  <div :class="'bg-brand ' + customPadding"  style="--nav-h: 10rem">
+  <div class="bg-brand md:px-20 px-5 md:px-20 py-5"  style="--nav-h: 10rem">
     <Navbar/>
     <RouterView />
   </div>
